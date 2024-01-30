2ae6cfea752ffdf73c3fcac3bb3a23ebd2d06633
Sadie Carroll <sadie.carroll@kellogg.northwestern.edu>
2024-01-30 16:22:51 +0000

    lab through models

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -11,14 +11,54 @@ Company.destroy_all
 # - Insert, read, update, and delete rows in companies table
 #   (i.e. full CRUD of company data).
 
+puts "There are #{Company.all.count} companies"
+
 # 1a. check out the schema file
 # 1b. check out the model file
 
 # 2. insert new rows in companies table
 
+new_company = Company.new
+
+puts new_company.inspect
+
+new_company["name"] = "Apple"
+new_company ["city"] = "Cupertino"
+new_company ["state"] = "CA"
+new_company ["url"] = "https.//www.apple.com"
+new_company.save
+
+new_company2 = Company.new
+
+new_company2["name"] = "Amazon"
+new_company2 ["city"] = "Seattle"
+new_company2 ["state"] = "WA"
+new_company2 ["url"] = "https.//www.amazon.com"
+new_company2.save
+puts "There are #{Company.all.count} companies"
+new_company.save
+
+new_company3 = Company.new
+
+new_company3["name"] = "Twitter"
+new_company3 ["city"] = "SF"
+new_company3 ["state"] = "CA"
+new_company3 ["url"] = "https.//www.twitter.com"
+new_company3.save
+puts "There are #{Company.all.count} companies"
+new_company.save
+
 # 3. query companies table to find all row with California company
 
+all_companies= Company.all
+puts all_companies.inspect 
+
+cali_companies = Company.where({"state" => "CA"})
+puts cali_companies.inspect
+puts "Companies in Cali: #{cali_companies.count}"
 # 4. query companies table to find single row for Apple
+
+
 
 # 5. read a row's column value
 
